30a40c0b7776d046e6f6a68bf3a1ec8a3b22ce44
luisvalen43 <70668041+luisvalen43@users.noreply.github.com>
2020-10-07 09:17:17 -0500

    Add files via upload

---
+++ cambiar.js
@@ -0,0 +1,55 @@
+
+var mostrar=function(){
+	var n1=document.getElementById('p');
+	var n2=document.getElementById('hp');
+	var n3=document.getElementById('hp2');
+	var n4=document.getElementById('hp3');
+	var n5=document.getElementById('hpm');
+
+	console.log("hola");
+	n5.className="mostrar3";
+	n1.className="cuadro1f";
+	n2.className="cuadro1f";
+	n3.className="cuadro1f";
+	n4.className="cuadro1f";
+
+}
+
+var definir=function(){
+	var n1=document.getElementById('p');
+	var n2=document.getElementById('hp');
+	var n3=document.getElementById('hp2');
+	var n4=document.getElementById('hp3');
+	var n5=document.getElementById('hpm');
+	var n6=document.getElementById('hpm2');
+
+	console.log("como esta");
+	n5.className="cuadro1f";
+	n1.className="cuadro1f";
+	n2.className="cuadro1f";
+	n3.className="cuadro1f";
+	n4.className="cuadro1f";
+	n6.className="quees2";
+
+
+
+}
+var solucion=function(){
+	var n1=document.getElementById('p');
+	var n2=document.getElementById('hp');
+	var n3=document.getElementById('hp2');
+	var n4=document.getElementById('hp3');
+	var n5=document.getElementById('hpm');
+	var n6=document.getElementById('hpm2');
+    var n7=document.getElementById('hpm3');
+
+	console.log("como esta");
+	n5.className="cuadro1f";
+	n1.className="cuadro1f";
+	n2.className="cuadro1f";
+	n3.className="cuadro1f";
+	n4.className="cuadro1f";
+	n6.className="cuadro1f";
+	n7.className="solu2";
+
+}
